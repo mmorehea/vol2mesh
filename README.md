bd4072ec1e457850c33d1fcd4298d676d5413c41
Michael Morehead <mmorehea@mix.wvu.edu>
2017-06-04 19:09:59 -0400

    updated readme, added scripts

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # vol2mesh
+
+A mesh generating wrapper in python3. Input is a list of folders containing 3D tiff stack. 
+
+Output is a directory for meshes. Third option is decimation percentage.
+
+Example usage: python3 vol2mesh.py ./data/ ./meshes/ .2
+
+
+Marching cubes implementation: https://github.com/ilastik/marching_cubes
+Mesh decimation implementation: https://github.com/sp4cerat/Fast-Quadric-Mesh-Simplification
+Reading TIFFs with tifffile: http://www.lfd.uci.edu/~gohlke/code/tifffile.py.html
---
+++ vol2mesh.py
@@ -0,0 +1,98 @@
+from marching_cubes import march
+import code
+import tifffile
+import numpy as np
+import glob
+from numpy import load
+
+from multiprocessing.dummy import Pool as ThreadPool
+import subprocess
+import queue
+import threading
+import os
+import sys
+import pickle
+
+SCALEX = 1.0
+SCALEY = 1.0
+SCALEZ = 1.0
+
+def findBBDimensions(listOfPixels):
+	xs = listOfPixels[0]
+	ys = listOfPixels[1]
+	zs = listOfPixels[2]
+
+	minxs = min(xs)
+	maxxs = max(xs)
+
+	minys = min(ys)
+	maxys = max(ys)
+
+	minzs = min(zs)
+	maxzs = max(zs)
+
+	dx = maxxs - minxs
+	dy = maxys - minys
+	dz = maxzs - minzs
+
+	return [minxs, maxxs+1, minys, maxys+1, minzs, maxzs+1], [dx, dy, dz]
+
+def calcMesh(stackname, labelStack, location, simplify):
+
+	indices = np.where(labelStack>0)
+	box, dimensions = findBBDimensions(indices)
+
+
+	window = labelStack[box[0]:box[1], box[2]:box[3], box[4]:box[5]]
+	localIndices = np.where(window > 0)
+	
+	paddedWindowSizeList = list(window.shape)
+	paddedWindowSize = tuple([i+2 for i in paddedWindowSizeList])   	  	
+		
+	blankImg = np.zeros(paddedWindowSize, dtype=bool)
+	
+	blankImg[tuple(([i+1 for i in localIndices[0]], [i+1 for i in localIndices[1]], [i+1 for i in localIndices[2]]))] = 1
+	print("Building mesh...")
+	vertices, normals, faces = march(blankImg.transpose(), 3)  # zero smoothing rounds
+	with open(location + os.path.basename(stackname) +".obj", 'w') as f:
+		f.write("# OBJ file\n")
+
+		for v in vertices:
+			f.write("v %.2f %.2f %.2f \n" % ((box[0] * SCALEX) + (v[2] * SCALEX), (box[2] * SCALEY) + (v[1] * SCALEY), (box[3] * SCALEZ) + v[0]))
+		for n in normals:
+			f.write("vn %.2f %.2f %.2f \n" % (n[2], n[1], n[0]))
+		for face in faces:
+			f.write("f %d %d %d \n" % (face[0]+1, face[1]+1, face[2]+1))
+	print("Decimating Mesh...")
+	if os.name == 'nt':
+		s = './binWindows/simplify ./' + location + os.path.basename(stackname) +".obj ./" + location + os.path.basename(stackname) +".smooth.obj " + str(simplify)
+	else:
+		s = './binLinux/simplify ./' + location + os.path.basename(stackname) +".obj ./" + location + os.path.basename(stackname) +".smooth.obj " + str(simplify)
+	print(s)
+	subprocess.call(s, shell=True)
+
+def main():
+	q = queue.Queue()
+	meshes = sys.argv[2]
+	simplify = sys.argv[3]
+	alreadyDone = glob.glob(meshes + "*.obj")
+	alreadyDone = [os.path.basename(i)[:-4] for i in alreadyDone]
+
+	labelsFolderPath = sys.argv[1]
+	labelsPaths = sorted(glob.glob(labelsFolderPath +'*'))
+	#code.interact(local=locals())
+	for ii,stack in enumerate(labelsPaths):
+		if os.path.basename(stack) in alreadyDone:
+			print("Detected already processed file. Skipping.")
+			print("[Delete file in output folder to reprocess.]")
+			continue
+		labelStack = tifffile.imread(stack)
+		#labelStack = np.dstack(labelStack)
+		print("Loaded data stack " + str(ii) + "/" + str(len(labelsPaths)))
+		print("Thresholding...")
+				
+		calcMesh(stack, labelStack, meshes, simplify)
+
+
+if __name__ == "__main__":
+	main()
